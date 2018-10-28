04befd9c0eb813ced4096bf808146d457a423dea
Daniel <dburbach1982@gmail.com>
2018-10-27 21:11:48 -0700

    tidied up HTML but still working on getting the form to stop and recognize name and order entries

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,7 @@ function Order(firstName, lastName, newSize, newIcecream, newToppings, newSauce)
     this.newToppings = newToppings,
     this.newSauce = newSauce
 }
-debugger;
+
 Order.prototype.fullOrder = function() {
   return this.firstName + " " + this.lastName + " " + this.newSize + " " + this.newIcecream + " " + this.newToppings + " " + this.newSauce
 }
@@ -27,58 +27,55 @@ Order.prototype.fullOrder = function() {
 // user interface logic
 $(document).ready(function() {
   event.preventDefault();
-  $("#submit").click(function() {
-    $("#orderName").append('<div class="form-group">' + '<label for="firstName">First name</label>' + '<input type="text" class="form-control" id="firstName">' + '</div>' + '<div class="form-group">' + '<label for="lastName">Last name</label>' + '<input type="text" class="form-control" id="lastName">' + '</div>');
-  });
-  debugger;
-  $("form#fullOrder").submit(function(event) {
-    $(".order-group").each(function() {
-      var inputtedFirstName = $(this).text("input#firstName").val();
-      var inputtedLastName = $(this).text("input#lastName").val();
-      var inputtedSize = $(this).find("input.newSize").val();
-      var inputtedIceCream = $(this).find("input.newIcecream").val();
-      var inputtedToppings = $(this).find("input.newToppings").val();
-      var inputtedSauces = $(this).find("input.newSauce").val();
-      var newOrder = new Order(inputtdFirstName, inputtdLastName, inputtedSize, inputtedIceCream, inputtedToppings, inputtedSauces)
-      newOrder.yourOrder.push(newOrder)
+    $("#submit").click(function() {
+      $("#orderName").append('<div class="form-group">' + '<label for="firstName">First name</label>' + '<input type="text" class="form-control" id="firstName">' + '</div>' + '<div class="form-group">' + '<label for="lastName">Last name</label>' + '<input type="text" class="form-control" id="lastName">' + '</div>');
     });
-
-    $("ul#orders").append("<li><span class='contact'>" + newOrder.fullOrder() + "</span></li>");
-
-    debugger;
-    $(".contact").last().click(function() {
-      $("#show-orders").show();
-      $("#show-orders h2").text(newContact.fullName());
-      $(".firstName").text(newContact.firstName);
-      $(".lastName").text(newContact.lastName);
-      $("ul#orders").text("");
-      newOrder.order.forEach(function(order) {
-        $("ul#orders").append("<li>"
-          order.firstName + ", " + order.lastName + ", " + order.newSize + ", " + order.newIceCream + ", " + order.newToppings + ", " + order.newSauce + "</li>");
+debugger;
+    $("form#fullOrder").click(function(event) {
+      $(".order-group").each(function() {
+        var inputtedFirstName = $(this).find("input#firstName").val();
+        var inputtedLastName = $(this).find("input#lastName").val();
+        var inputtedSize = $(this).find("input.newSize").val();
+        var inputtedIceCream = $(this).find("input.newIcecream").val();
+        var inputtedToppings = $(this).find("input.newToppings").val();
+        var inputtedSauces = $(this).find("input.newSauce").val();
+        var newOrder = new Order(inputtdFirstName, inputtdLastName, inputtedSize, inputtedIceCream, inputtedToppings, inputtedSauces)
+        newOrder.yourOrder.push(newOrder)
       });
-    });
-  })
-  debugger;
 
-  function updateTotal(e) {
-    var form = this.form;
-    var val = parseFloat(form.elements['total'].value);
-    if (this.checked) {
-      val += parseFloat(this.value);
-    } else {
-      val -= parseFloat(this.value);
-    }
-    form.elements['total'].value = formatDecimal(val);
-  }
-  debugger;
+      $("ul#orders").append("<li><span class='contact'>" + newOrder.fullOrder() + "</span></li>");
 
-  function formatDecimal(val, n) {
-    n = n || 2;
-    var str = "" + Math.round(parseFloat(val) * Math.pow(10, n));
-    while (str.lenght <= n) {
-      str = "0" + str;
+debugger;
+      $(".contact").last().click(function() {
+        $("#show-orders").show();
+        $("#show-orders h2").text(newContact.fullName());
+        $(".firstName").text(newContact.firstName);
+        $(".lastName").text(newContact.lastName);
+        $("ul#orders").text("");
+        newOrder.order.forEach(function(order) {
+          $("ul#orders").append("<li>" + order.firstName + ", " + order.lastName + ", " + order.newSize + ", " + order.newIceCream + ", " + order.newToppings + ", " + order.newSauce + "</li>");
+        });
+      });
+    })
+
+    function updateTotal(e) {
+      var form = this.form;
+      var val = parseFloat(form.elements['.total'].value);
+      if (this.checked) {
+        val += parseFloat(this.value);
+      } else {
+        val -= parseFloat(this.value);
+      }
+      form.elements['.total'].value = formatDecimal(val);
     }
-    var pt = str.lenght - n;
-    return str.slice(0, pt) + "." + str.slice(pt);
-  }
+
+    function formatDecimal(val, n) {
+      n = n || 2;
+      var str = "" + Math.round(parseFloat(val) * Math.pow(10, n));
+      while (str.lenght <= n) {
+        str = "0" + str;
+      }
+      var pt = str.lenght - n;
+      return str.slice(0, pt) + "." + str.slice(pt);
+    }
 });
