287002ca2c96db0ca0dff8ea2bfc512f590f944d
Daniel <dburbach1982@gmail.com>
2018-10-27 19:48:35 -0700

    added functions of JS to try to save name and order AND return it in the form on the right side

--- js/scripts.js
+++ js/scripts.js
@@ -3,19 +3,32 @@ function Contact(firstName, lastName) {
   this.firstName = firstName,
   this.lastName = lastName
 }
+
+function Order(newSize, newIcecream, newToppings, newSauce) {
+  this.newSize = newSize,
+  this.newIcecream = newIcecream,
+  this.newToppings = newToppings,
+  this.newSauce = newSauce
+}
+
+Order.prototype.fullOrder = function() {
+  return this.newSize + " " + this.newIcecream + " " + this.newToppings + " " + this.newSauce
+}
+
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName
 }
 
-function attachCheckbox() {
-  var order = document.getElementById('order-group');
-  var tops = order.getElementByTagName('input');
-  for (var i=0, len=tops.length; i<len; i++) {
-    if (tops[i].type === 'checkbox') {
-      tops[i].onclick = updateTotal;
-    }
-  }
-}
+
+//function attachCheckbox() {
+//  var order = document.getElementById('order-group');
+//  var tops = order.getElementByTagName('input');
+//  for (var i=0, len=tops.length; i<len; i++) {
+//    if (tops[i].type === 'checkbox') {
+//      tops[i].onclick = updateTotal;
+//    }
+//  }
+//}
 
 
 // user interface logic
@@ -24,7 +37,6 @@ $(document).ready(function() {
   $("#submit").click(function() {
     $("#orderName").append('<div class="form-group">' + '<label for="firstName">First name</label>' + '<input type="text" class="form-control" id="firstName">' + '</div>' + '<div class="form-group">' + '<label for="lastName">Last name</label>' + '<input type="text" class="form-control" id="lastName">' + '</div>');
     });
-  });
 
   $("form#fullOrder").submit(function(event) {
     event.preventDefault();
@@ -33,15 +45,27 @@ $(document).ready(function() {
     var inputtedLastName = $("input#lastName").val();
     var newName = new Name(inputtdFirstName, inputtdLastName);
 
-    $(".order-group").each(function() {
-        var inputtedSize = $(this).find("input.new-size").val();
-        var inputtedIceCream = $(this).find("input.new-icecream").val();
-        var inputtedToppings = $(this).find("input.new-toppings").val();
-        var inputtedSauces = $(this).find("input.new-sauce").val();
-        var newOrder = new Order (inputtedSize, inputtedIceCream, inputtedToppings, inputtedSauces)
-        newName.
-    })
+  $(".order-group").each(function() {
+      var inputtedSize = $(this).find("input.newSize").val();
+      var inputtedIceCream = $(this).find("input.newIcecream").val();
+      var inputtedToppings = $(this).find("input.newToppings").val();
+      var inputtedSauces = $(this).find("input.newSauce").val();
+      var newOrder = new Order (inputtedSize, inputtedIceCream, inputtedToppings, inputtedSauces)
+      newName.yourOrder.push(newOrder)
+  });
+  $("ul#orders").append("<li><span class='contact'>" + newContact.fullName() + newOrder.fullOrder() + "</span> + </li>");
 
+  $(".o")
+
+  $(".contact").last().click(function() {
+    $("#show-orders").show();
+    $("#show-orders h2").text(newContact.fullName());
+    $(".firstName").text(newContact.firstName);
+    $(".lastName").text(newContact.lastName);
+    $("ul#orders").text("");
+    newOrder.order.forEach(function(order) {
+      $("ul#orders").append("<li>" + order.newSize + ", " + order.newIceCream + ", " + order.newToppings + ", " + order.newSauce + "</li>");
+    });
   });
 
   function updateTotal(e) {
